595141fb6c19d83c7c007b99db3c6ed6ab69cf96
dom (@frogweezer) <dompec07@gmail.com>
2021-12-16 11:31:25 -0500

    bruh

--- scripts/json_get.js
+++ scripts/json_get.js
@@ -1,14 +1,36 @@
-function rebuildAll(className) {
-  var x = document.getElementsByClassName(className);
-  var i;
-  for (i = 0; i < x.length; i++) {
-    var node = x[i];
-    var x = node.childNodes;
+function rebuildFromArray(array, apiData) {
+  var dexCount = 0;
+  for (var i = 0; i < array.length; i++) {
+    var type = array[i];
 
-    for (let item of x) {
-      var className = item.getAttributeNode("cellType");
-      if (className === "pokemonColumn") {
-        console.log(item);
+    var elements = document.getElementsByClassName(type);
+    for (var y = 0; y < elements.length; y++) {
+      var element = elements[y];
+      var className = element.className;
+
+      if (className == "pokemonArt") {
+        dexCount++;
+        var trueCount = 0;
+        var trueLength = dexCount.toString().length
+
+        if (trueLength == 1) {
+          var trueCount = "00" + dexCount;
+        } else if (trueLength == 2) {
+          trueCount = "0" + dexCount;
+        } else {
+          trueCount = dexCount;
+        }
+
+        $.get(apiData["baseUrl"] + "pokemon/" + dexCount, function(data) {
+          var pokemonData = [];
+          pokemonData["name"] = data["name"];
+          pokemonData["id"] = trueCount;
+          console.log(pokemonData);
+        });
+
+
+      } else if (className == "pokemonData") {
+      } else {
       }
     }
   }
@@ -25,5 +47,5 @@ JSONget("api/api_data.json").then(function(apiData) {
   // JSON Data is stored in "apiData" variable
   // console.log(apiData);
 
-  rebuildAll("pokemonRow");
+  rebuildFromArray(["pokemonArt", "pokemonData"], apiData);
 });
