0bd86af09b926489efce2b132942fe60ea24c2c7
dom <dompec07@gmail.com>
2022-02-07 08:46:10 -0500

    oh my god

--- scripts/json_get.js
+++ scripts/json_get.js
@@ -1,123 +1,121 @@
-var HTMLConfig = {
-  "dex": {
+var Config = {
     "columnCount": 3
-  }
 }
 
 function capitalizeString(string) {
-  return string.replace(/^\w/, (c) => c.toUpperCase());
+    return string.replace(/^\w/, (c) => c.toUpperCase());
 };
 
 function removeQuotes(string) {
-  return string.replace(/['"]+/g, '');
+    return string.replace(/['"]+/g, '');
 };
 
 
 function setJSON(apiData, dexCount, trueCount, element, nationalDexData) {
-  var currentUrl = apiData["baseUrl"] + "pokemon/" + dexCount;
+    var currentUrl = apiData.baseUrl + "pokemon/" + dexCount;
 
-  // check if URL exists
-  $.get(currentUrl).done(function (data) {
-    var trueName = capitalizeString(data["name"]);
+    // check if URL exists
+    $.get(currentUrl).done(function (data) {
+        var trueName = capitalizeString(data.name);
 
-    /*
 
-    console.log(
-      "(" + trueCount + "/" +
-      nationalDexData["pokemon_entries"]["length"] +
-      ") Generating info for " + trueName
-    );
 
-    */
+        console.log(
+            "(" + trueCount + "/" +
+            nationalDexData.pokemon_entries.length +
+            ") Generating info for " + trueName
+        );
 
-    var artElement = element.getElementsByClassName("pokemonArt")[0];
-    var cellArtImage = artElement.getElementsByClassName("cellArtImage")[0];
-    cellArtImage.src = data["sprites"]["front_default"];
 
-    var dataElement = element.getElementsByClassName("pokemonData")[0];
-    dataElement.getElementsByClassName("pokemonName")[0].innerHTML = trueName;
-    dataElement.getElementsByClassName("pokemonId")[0].innerHTML = "#" + trueCount;
 
-    // Set types
-    var metadataElement = dataElement.getElementsByClassName("pokemonMetaData")[0];
-    var types = data["types"];
-    var typeCount = types["length"];
+        var artElement = element.getElementsByClassName("pokemonArt")[0];
+        var cellArtImage = artElement.getElementsByClassName("cellArtImage")[0];
+        cellArtImage.src = data.sprites.front_default;
 
-    for (var i = 0; i < typeCount; i++) {
-      var type = types[i];
-      var theType = type["type"];
+        var dataElement = element.getElementsByClassName("pokemonData")[0];
+        dataElement.getElementsByClassName("pokemonName")[0].innerHTML = trueName;
+        dataElement.getElementsByClassName("pokemonId")[0].innerHTML = "#" + trueCount;
 
-      metadataElement.innerHTML +=
-      capitalizeString(
-        removeQuotes(
-          JSON.stringify(theType["name"])
-        )
-      );
+        // Set types
+        var metadataElement = dataElement.getElementsByClassName("pokemonMetaData")[0];
+        var types = data.types;
+        var typeCount = types.length;
 
-      if (typeCount != 1  &&  i != typeCount - 1) {
-        metadataElement.innerHTML += ", ";
-      };
-    };
+        for (var i = 0; i < typeCount; i++) {
+            var type = types[i];
+            var theType = type.type;
 
-    // Set evolutions
-    $.get("https://pokeapi.co/api/v2/pokemon-species/" + dexCount).then(function(speData) {
-      $.get(speData["evolution_chain"]["url"]).then(function(evoData) {
-        metadataElement.innerHTML += "<br>Species #" + JSON.stringify(evoData.id);
-      });
+            metadataElement.innerHTML +=
+                capitalizeString(
+                    removeQuotes(
+                        JSON.stringify(theType.name)
+                    )
+                );
+
+            if (typeCount != 1 && i != typeCount - 1) {
+                metadataElement.innerHTML += ", ";
+            };
+        };
+
+        // Set evolutions
+        $.get("https://pokeapi.co/api/v2/pokemon-species/" + dexCount).then(function (speData) {
+            $.get(speData.evolution_chain.url).then(function (evoData) {
+                metadataElement.innerHTML += "<br>Species #" + JSON.stringify(evoData.id);
+            });
+        });
+
+        //console.log(" » complete!");
+    }).fail(function () {
+        //console.log(trueCount + " does not exist");
     });
-
-    //console.log(" » complete!");
-  }).fail(function () {
-    //console.log(trueCount + " does not exist");
-  });
 }
 
 
-$.get("api/api_data.json").then(function(apiData) {
-  // JSON Data is stored in "apiData" variable
+$.get("api/api_data.json").then(function (apiData) {
+    // JSON Data is stored in "apiData" variable
 
 
-  // Get national dex info to fill info for all pokemon
-  $.get("https://pokeapi.co/api/v2/pokedex/1").then(function(nationalDexData) {
-    var pokedexLength = nationalDexData["pokemon_entries"]["length"];
-    var repeatRows = Math.ceil(pokedexLength / 3);
+    // Get national dex info to fill info for all pokemon
+    $.get("https://pokeapi.co/api/v2/pokedex/1").then(function (nationalDexData) {
+        var pokedexLength = nationalDexData.pokemon_entries.length;
+        var repeatRows = Math.ceil(pokedexLength / 3);
 
-    // Create columns
-    for (var i = 0; i < HTMLConfig["dex"]["columnCount"] - 1; i++) {
-      var cl = document.getElementsByClassName("pokedexCell")[0].cloneNode(true);
-      document.getElementsByClassName("pokedexRow")[0].appendChild(cl);
-    }
+        // Create columns
+        for (var i = 0; i < Config.columnCount - 1; i++) {
+            var cl = document.getElementsByClassName("pokedexCell")[0].cloneNode(true);
+            document.getElementsByClassName("pokedexRow")[0].appendChild(cl);
+        }
 
-    var tr = document.getElementsByClassName("pokedexRow")[0];
+        var tr = document.getElementsByClassName("pokedexRow")[0];
 
-    for (var i = 0; i < repeatRows - 1; i++) {
-      var cl = tr.cloneNode(true);
-      document.getElementById("pokedexTableBody").appendChild(cl);
-    }
+        for (var i = 0; i < repeatRows - 1; i++) {
+            var cl = tr.cloneNode(true);
+            document.getElementById("pokedexTableBody").appendChild(cl);
+        }
 
 
-    // Create rows
-    var dexCount = 0;
+        // Create rows
+        var dexCount = 0;
 
-    var array = document.getElementById("pokedexTable");
-    var elements = array.getElementsByClassName("pokemonRow");
+        var array = document.getElementById("pokedexTable");
+        var elements = array.getElementsByClassName("pokemonRow");
 
-    for (var i = 0; i < elements.length; i++) {
-      var element = elements[i]
-      dexCount++;
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i]
+            dexCount++;
 
-      var trueCount = 0;
-      var trueLength = dexCount.toString().length
+            var trueCount = 0;
+            var trueLength = dexCount.toString().length
 
-      if (trueLength == 1) {
-        var trueCount = "00" + dexCount;
-      } else if (trueLength == 2) {
-        trueCount = "0" + dexCount;
-      } else {
-        trueCount = dexCount;
-      }
+            if (trueLength == 1) {
+                var trueCount = "00" + dexCount;
+            } else if (trueLength == 2) {
+                trueCount = "0" + dexCount;
+            } else {
+                trueCount = dexCount;
+            }
 
-      setJSON(apiData, dexCount, trueCount, element, nationalDexData);
-    };
-  });
+            setJSON(apiData, dexCount, trueCount, element, nationalDexData);
+        };
+    });
 });
